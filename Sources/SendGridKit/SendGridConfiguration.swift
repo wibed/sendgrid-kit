84c4fdd5b0a0bc0836186c89cf82e98c42851487
wibed <wibe@tutanota.com>
2020-10-01 15:44:11 +0200

    add: redefined optional argument

--- Sources/SendGridKit/SendGridConfiguration.swift
+++ Sources/SendGridKit/SendGridConfiguration.swift
@@ -5,18 +5,19 @@ import NIOHTTP1
 
 
 public struct SendGridConfiguration {
-    public var apiURL: String = ProcessInfo.processInfo
-        .environment["SENDGRID_URL"] ?? "https://api.sendgrid.com/v3/mail/send"
-    
+    public var apiURL: String
     public var apiKey: String
     
     public init(
-        apiURL: String?,
-        apiKey: String = ProcessInfo.processInfo.environment["SENDGRID_KEY"]!
+        apiURL: String = ProcessInfo.processInfo.environment["SENDGRID_URL"] ??
+            "https://api.sendgrid.com/v3/mail/send",
+        apiKey: String? = ProcessInfo.processInfo.environment["SENDGRID_KEY"]
     ){
-        if let apiURL = apiURL {
-            self.apiURL = apiURL
+        guard let apiKey = apiKey else {
+            fatalError("APIKey not found.")
         }
+        
         self.apiKey = apiKey
+        self.apiURL = apiURL
     }
 }
