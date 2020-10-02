da7b1d7096c14ab412b1b2f15e26b136d0bcdf2c
wibed <wibe@tutanota.com>
2020-10-02 15:22:25 +0200

    chg: stripped any logic interpreting meta information or assumptions

--- Sources/SendGridKit/SendGridClient.swift
+++ Sources/SendGridKit/SendGridClient.swift
@@ -39,61 +39,77 @@ public struct SendGridClient {
     
     
     /// Send to endpoint
-    public func send(emails: [SendGridEmail], on eventLoop: EventLoop) ->
-    EventLoopFuture<Void> {
-        
-        let futures = emails.map { email
-            -> EventLoopFuture<Void>  in
+    public func mailRequest(email: SendGridEmail) -> EventLoopFuture<HTTPClient.Request> {
+        do {
+            let method: HTTPMethod = .POST
+            var headers = HTTPHeaders()
+            headers.add(name: "Authorization", value: "Bearer \(self.config!.apiKey)")
+            headers.add(name: "Content-Type", value: "application/json")
+            let url = self.config!.apiURL
             
-            do {
-                var headers = HTTPHeaders()
-                headers.add(name: "Authorization", value: "Bearer \(self.config!.apiKey)")
-                headers.add(name: "Content-Type", value: "application/json")
-                
-                let bodyData = try encoder.encode(email)
-                
-                let bodyString = String(decoding: bodyData, as: UTF8.self)
-                
-                let request = try HTTPClient.Request(url: self.config!.apiURL,
-                                                     method: .POST,
-                                                     headers: headers,
-                                                     body: .string(bodyString))
-                
-                return self._send(request).flatMap{ response in
-                    
-                    switch response.status {
-                    case .ok, .accepted:
-                        return eventLoop.makeSucceededFuture(())
-                    default:
-                        let byteBuffer = response.body ?? ByteBuffer(.init())
-                        let responseData = Data(byteBuffer.readableBytesView)
-                    
-                        do {
-                            let error = try self.decoder.decode(SendGridError.self,
-                                                                from: responseData)
-                            return eventLoop.makeFailedFuture(error)
-                        } catch {
-                            return eventLoop.makeFailedFuture(error)
-                        }
-                    }
-                }
-            } catch {
-                return eventLoop.makeFailedFuture(error)
-            }
+            let body = try encoder.encode(email)
+            
+            return eventLoop.makeSucceededFuture(
+                try HTTPClient.Request(url: url,
+                               method: method,
+                               headers: headers,
+                               body: .data(body))
+            )
+        } catch {
+            return eventLoop.makeFailedFuture(error)
         }
-        
-        return EventLoopFuture<Void>.andAllSucceed(futures, on: eventLoop)
     }
+        
+//        let futures = emails.map { email
+//            -> EventLoopFuture<Void>  in
+//
+//            do {
+//                var headers = HTTPHeaders()
+//                headers.add(name: "Authorization", value: "Bearer \(self.config!.apiKey)")
+//                headers.add(name: "Content-Type", value: "application/json")
+//
+//                let bodyData = try encoder.encode(email)
+//
+//                let bodyString = String(decoding: bodyData, as: UTF8.self)
+//
+//                let request = try HTTPClient.Request(url: self.config!.apiURL,
+//                                                     method: .POST,
+//                                                     headers: headers,
+//                                                     body: .string(bodyString))
+//
+//                return self._send(request).flatMap{ response in
+//
+//                    switch response.status {
+//                    case .ok, .accepted:
+//                        return eventLoop.makeSucceededFuture(())
+//                    default:
+//                        let byteBuffer = response.body ?? ByteBuffer(.init())
+//                        let responseData = Data(byteBuffer.readableBytesView)
+//
+//                        do {
+//                            let error = try self.decoder.decode(SendGridError.self,
+//                                                                from: responseData)
+//                            return eventLoop.makeFailedFuture(error)
+//                        } catch {
+//                            return eventLoop.makeFailedFuture(error)
+//                        }
+//                    }
+//                }
+//            } catch {
+//                return eventLoop.makeFailedFuture(error)
+//            }
+//        }
+//
+//        return EventLoopFuture<Void>.andAllSucceed(futures, on: eventLoop)
+//    }
     
     
     
-    private func _send(_ sendgrid: HTTPClient.Request)
+    private func execute(_ sendgrid: HTTPClient.Request)
     -> EventLoopFuture<HTTPClient.Response> {
         return self.http.execute(
             request: sendgrid,
             eventLoop: .delegate(on: self.eventLoop)
-        ).map { response in
-                response
-        }
+        ).map { $0 }
     }
 }
