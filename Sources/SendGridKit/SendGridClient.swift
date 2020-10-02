e86b29252498e4509b89f7b024ad2169c46ae4cc
wibed <wibe@tutanota.com>
2020-10-02 15:34:06 +0200

    rmv: cleaned up comments and execute now public

--- Sources/SendGridKit/SendGridClient.swift
+++ Sources/SendGridKit/SendGridClient.swift
@@ -59,53 +59,8 @@ public struct SendGridClient {
             return eventLoop.makeFailedFuture(error)
         }
     }
-        
-//        let futures = emails.map { email
-//            -> EventLoopFuture<Void>  in
-//
-//            do {
-//                var headers = HTTPHeaders()
-//                headers.add(name: "Authorization", value: "Bearer \(self.config!.apiKey)")
-//                headers.add(name: "Content-Type", value: "application/json")
-//
-//                let bodyData = try encoder.encode(email)
-//
-//                let bodyString = String(decoding: bodyData, as: UTF8.self)
-//
-//                let request = try HTTPClient.Request(url: self.config!.apiURL,
-//                                                     method: .POST,
-//                                                     headers: headers,
-//                                                     body: .string(bodyString))
-//
-//                return self._send(request).flatMap{ response in
-//
-//                    switch response.status {
-//                    case .ok, .accepted:
-//                        return eventLoop.makeSucceededFuture(())
-//                    default:
-//                        let byteBuffer = response.body ?? ByteBuffer(.init())
-//                        let responseData = Data(byteBuffer.readableBytesView)
-//
-//                        do {
-//                            let error = try self.decoder.decode(SendGridError.self,
-//                                                                from: responseData)
-//                            return eventLoop.makeFailedFuture(error)
-//                        } catch {
-//                            return eventLoop.makeFailedFuture(error)
-//                        }
-//                    }
-//                }
-//            } catch {
-//                return eventLoop.makeFailedFuture(error)
-//            }
-//        }
-//
-//        return EventLoopFuture<Void>.andAllSucceed(futures, on: eventLoop)
-//    }
     
-    
-    
-    private func execute(_ sendgrid: HTTPClient.Request)
+    public func execute(_ sendgrid: HTTPClient.Request)
     -> EventLoopFuture<HTTPClient.Response> {
         return self.http.execute(
             request: sendgrid,
